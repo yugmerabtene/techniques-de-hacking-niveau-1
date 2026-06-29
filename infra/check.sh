0ffa2e17368507b57706640605d97e6b7edb936e
yugmerabtene <yug.merabtene@gmail.com>
2026-06-29 20:37:35 +0200

    rm infra

--- infra/check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# =====================================================================
-# check.sh — Wrapper qui exécute check_all_services.sh avec docker
-# =====================================================================
-exec newgrp docker <<< "$(dirname "$0")/check_all_services.sh"
