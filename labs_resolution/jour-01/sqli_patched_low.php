c47d7e4ccd26c88e5fc9caef8420217073c221c2
yugmerabtene <yug.merabtene@gmail.com>
2026-06-30 17:32:26 +0200

    feat(jour1): correctif SQLi requêtes préparées + script de correction reproductible

---
+++ labs_resolution/jour-01/sqli_patched_low.php
@@ -0,0 +1,22 @@
+<?php
+
+if( isset( $_REQUEST[ 'Submit' ] ) ) {
+    $id = $_REQUEST[ 'id' ];
+
+    $stmt = mysqli_prepare($GLOBALS["___mysqli_ston"],
+        "SELECT first_name, last_name FROM users WHERE user_id = ?");
+    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if( $result ) {
+        while( $row = mysqli_fetch_assoc( $result ) ) {
+            $first = $row["first_name"];
+            $last  = $row["last_name"];
+            $html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+        }
+    }
+    mysqli_stmt_close($stmt);
+}
+
+?>
